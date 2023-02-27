458d913afc3f560c4d98113c51a0556c6deeda24
samanxsy <samanxsy@gmail.com>
2023-02-27 21:44:09 +0100

    preparing for CI pipeline

--- app/Fruitvice.py
+++ app/Fruitvice.py
@@ -1,4 +1,3 @@
-import json
 import requests
 import pandas as pd
 
@@ -6,7 +5,7 @@ def fruits_data(fruit):
   """
   This functions receives the Fruit name as input, and will sort out the information about the fruit
   """
-  
+
   url = f"https://fruityvice.com/api/fruit/{fruit}"
   response = requests.get(url)
   data = response.json()
@@ -17,24 +16,24 @@ def fruits_data(fruit):
 
   df = pd.DataFrame(information)
   df2 = pd.DataFrame(nutrition)
-  
+
   information_table = df.to_html(index=False, classes="info_table")
   nutrition_table = df2.to_html(index=False, classes="ntr_table")
-  
+
   full_table = f"""
   <div class="about">About</div>
   {information_table}
   <div class="ntr_facts">Nutrition Facts (in 100g)</div>
   {nutrition_table}
   """
-  
+
   return full_table
 
 def fruit_mix(*fruits):
   """
   This function creates a table of all the selected fruits, and sums all the nutritions
   """
-  
+
   fruit_table = []
   for fruit in fruits:
     url = f"https://fruityvice.com/api/fruit/{fruit}"
@@ -42,20 +41,20 @@ def fruit_mix(*fruits):
     data = response.json()
     ntr = data['nutritions']
     fruit_table.append({'Name': data['name'], 'Carbohydrates': ntr['carbohydrates'], 'Protein': ntr['protein'], 'Fat': ntr['fat'], 'Calories': ntr['calories'], 'Sugar': ntr['sugar']})
-    
+
   df3 = pd.DataFrame(fruit_table)
   df3 = df3.append({'Name': 'Total', 'Carbohydrates': df3['Carbohydrates'].sum(), 'Protein': df3['Protein'].sum(), 'Fat': df3['Fat'].sum(), 'Calories': df3['Calories'].sum(), 'Sugar': df3['Sugar'].sum()}, ignore_index=True)
-    
+
   mix_table = df3.to_html(index=False, classes="mix_table")
-  
+
   return mix_table
 
 def sorting(*fruits, nutrition=None):
-  
+
   """
   This function will sort the table based on user need
   """
-  
+
   fruit_table = []
   for fruit in fruits:
     url = f"https://fruityvice.com/api/fruit/{fruit}"
@@ -67,9 +66,9 @@ def sorting(*fruits, nutrition=None):
   df3 = pd.DataFrame(fruit_table)
   if nutrition:
     df3 = df3.sort_values(by=nutrition, ascending=False)
-    
+
   df3 = df3.append({'Name': 'Total', 'Carbohydrates': df3['Carbohydrates'].sum(), 'Protein': df3['Protein'].sum(), 'Fat': df3['Fat'].sum(), 'Calories': df3['Calories'].sum(), 'Sugar': df3['Sugar'].sum()}, ignore_index=True)
-    
+
   mix_table = df3.to_html(index=False, classes="mix_table")
-  
+
   return mix_table
--- app/server.py
+++ app/server.py
@@ -7,41 +7,41 @@ app = Flask("Fruit World", static_folder="./app/static")
 
 @app.route('/')
 def home():
-  
+
   """
   The main page 
   """
-    
+
   page = ""
   f = open("app/templates/layout.html", "r")
   page = f.read()
   f.close()
-  
+
   fruit_info = Fruitvice.fruits_data("Apple")
   page = page.replace("Tap a fruit!", fruit_info)
-  
+
   return page
 
 @app.route('/fruits')
 def fruits():
-  
+
   """ 
   This function adds the new fruits to the table
   """
-  
+
   page = ""
   f = open("app/templates/layout.html", "r")
   page = f.read()
   f.close()
-  
+
   fruit = request.args.get("fruit")
-  
+
   if fruit not in fruit_list:
     fruit_list.append(fruit)
-    
+
   fruit_info = Fruitvice.fruits_data(fruit)
   mix_fruits = Fruitvice.fruit_mix(*fruit_list)
-  
+
   page = page.replace("Tap a fruit!", fruit_info)
   page = page.replace("Tap more fruits!", mix_fruits)
 
@@ -49,67 +49,64 @@ def fruits():
 
 @app.route('/re_sort')
 def re_sort():
-  
+
   """ 
   This function re-sorts the table based on user decision
   """
-  
+
   page = ""
   f = open("app/templates/layout.html", "r")
   page = f.read()
   f.close()
-  
+
   sort_type = request.args.get("sorting_type")
   mix_fruits = Fruitvice.fruit_mix(*fruit_list)
   fruit = fruit_list[1]
   fruit_info = Fruitvice.fruits_data(fruit)
-  
+
   if sort_type == "Protein":
     sort_table = Fruitvice.sorting(*fruit_list, nutrition="Protein")
     page = page.replace("Tap more fruits!", sort_table)
-    
+
   elif sort_type == "Sugar": 
     sort_table = Fruitvice.sorting(*fruit_list, nutrition="Sugar")
-    
+
     page = page.replace("Tap a fruit!", fruit_info)
     page = page.replace("Tap more fruits!", sort_table)
-    
+
   elif sort_type == "Carbohydrates": 
     sort_table = Fruitvice.sorting(*fruit_list, nutrition="Carbohydrates")
-    
+
     page = page.replace("Tap a fruit!", fruit_info)
     page = page.replace("Tap more fruits!", sort_table)
-    
+
   elif sort_type == "Calories":
     sort_table = Fruitvice.sorting(*fruit_list, nutrition="Calories")
-    
+
     page = page.replace("Tap a fruit!", fruit_info)
     page = page.replace("Tap more fruits!", sort_table)
-    
+
   elif sort_type == "Fat": 
     sort_table = Fruitvice.sorting(*fruit_list, nutrition="Fat")
-    
+
     page = page.replace("Tap a fruit!", fruit_info)
     page = page.replace("Tap more fruits!", sort_table)
-  
+
   else:
-    
+
     page = page.replace("Tap a fruit!", fruit_info)
     page = page.replace("Tap more fruits!", mix_fruits)
-  
+
   return page
-  
+
 @app.route('/reset')
 def reset_list():
-  
+
   """ 
   This function resets the list
   """
-  
+
   global fruit_list
   fruit_list = []
-  
-  return home()
 
-if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=5000)
+  return home()
